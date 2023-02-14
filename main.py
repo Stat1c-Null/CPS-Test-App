a03fb4104f04a247ae99f7ca983a6e013cbb346c
Mikita Slabysh <slabysh2015@gmail.com>
2023-02-14 04:58:01 -0500

    Improved UI, working on functionality

--- main.py
+++ main.py
@@ -1,20 +1,69 @@
-import pyautogui
 import tkinter as tk
+from tkinter import messagebox
 
-root = tk.Tk()
+class GUI:
 
-win_width = 400
-win_height = 500
-clicks = 0
+  def __init__(self):
+    #Create window
+    self.root = tk.Tk()
+    self.root.geometry("500x300")#Resolution
+    self.root.title("CPS")#App Title 
 
-bg = tk.Canvas(root, width=win_width, height=win_width)
-bg.pack()
+    #Vars
+    self.clicks = 0
+    self.timer = 0
+    self.score = 0
 
-#Make buttons
-button1 = tk.Button(text="CLICK ME", width=5, height=5, font=("Helvetica", 30), background="lime", padx=400, pady=100)
-clickCount = tk.Label(text=str(clicks) + " Clicks")
-clickCount.place(x=50, y=100)
+    #Texts
+    self.title = tk.Label(self.root, text="Clicks-per-Second", font=('Arial', 20))
+    self.title.pack(pady=15)
 
-bg.create_window(win_width, win_width, window=button1)
+    self.statsLabel = tk.Label(self.root, text=f"Clicks/s: {self.clicks} Score: {self.score} Timer: {self.timer}", font=('Arial', 14))
+    self.statsLabel.pack(pady=5)
 
-root.mainloop()
+    #Set columns for buttons in one row
+    self.timerButtonFrame = tk.Frame(self.root)
+    self.timerButtonFrame.columnconfigure(0, weight=1)
+    self.timerButtonFrame.columnconfigure(1, weight=1)
+    self.timerButtonFrame.columnconfigure(2, weight=1)
+    self.timerButtonFrame.columnconfigure(3, weight=1)
+
+    #Timer Buttons
+    self.oneSecBtn = tk.Button(self.timerButtonFrame, text="1 Second", font=('Arial', 10), command=self.changeTimer)
+    self.oneSecBtn.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+    self.fiveSecBtn = tk.Button(self.timerButtonFrame, text="5 Seconds", font=('Arial', 10), command=self.changeTimer)
+    self.fiveSecBtn.grid(row=0, column=1, sticky=tk.W+tk.E)
+
+    self.tenSecBtn = tk.Button(self.timerButtonFrame, text="10 Seconds", font=('Arial', 10), command=self.changeTimer)
+    self.tenSecBtn.grid(row=0, column=2, sticky=tk.W+tk.E)
+
+    self.fifthTeenSecBtn = tk.Button(self.timerButtonFrame, text="15 Seconds", font=('Arial', 10), command=self.changeTimer)
+    self.fifthTeenSecBtn.grid(row=0, column=3, sticky=tk.W+tk.E)
+
+    self.timerButtonFrame.pack(fill='x', padx=10, pady=10)
+
+    #Click button
+    self.clickButton = tk.Button(self.root, text="Click to Start", font=("Arial", 16), command=self.changeScore)
+    self.clickButton.pack(pady=20)
+
+    self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+    self.root.mainloop()
+
+  def changeScore(self):
+    self.score += 1
+    self.calculateCPS()
+
+  def changeTimer(self):
+    pass
+
+  def calculateCPS(self):
+    pass
+
+  #Prevent accidental closing
+  def on_closing(self):
+    if messagebox.askyesno(title="Quit?", message="Are you sure you wanna quit now ?"):
+      self.root.destroy()
+
+
+GUI()
