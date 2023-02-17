d9b5fa4e9a575a6fb4018d87f43e418c5dc6a40f
Mikita Slabysh <slabysh2015@gmail.com>
2023-02-17 09:44:58 -0500

    Small improvements

--- main.py
+++ main.py
@@ -2,88 +2,89 @@ import tkinter as tk
 from tkinter import messagebox
 import time
 
-class GUI:
+#Create window
+root = tk.Tk()
+root.geometry("500x300")#Resolution
+root.title("CPS")#App Title
+root.configure(bg="#18122B")#Background color
 
-  def __init__(self):
-    #Create window
-    self.root = tk.Tk()
-    self.root.geometry("500x300")#Resolution
-    self.root.title("CPS")#App Title
-    self.root.configure(bg="#18122B")#Background color
+#Vars
+clicks = 0
+timer = 0
+score = 0
+CPSOn = False
 
-    #Vars
-    self.clicks = 0
-    self.timer = 0
-    self.score = 0
-    self.CPSOn = False
+#Functions
+def updateStats():
+  global CPSOn, timer, score, clicks, statsLabel
+  if timer > 0:
+    CPSOn = True
+    score += 1
+    print(f"{timer} {CPSOn}")
+    statsLabel.configure(text=f"Clicks/s: {clicks} Score: {score} Timer: {timer}")
+    calculateCPS()
+  elif timer == 0:
+    CPSOn = False
 
-    #Texts
-    self.title = tk.Label(self.root, text="Clicks-per-Second", font=('Comic Sans MS', 20), background="#BFACE2", foreground="#18122B", borderwidth=5)
-    self.title.pack(pady=15)
+#Change Timer Time
+def changeTimer(time):
+  global clicks, score, timer, statsLabel
+  timer = time
+  statsLabel.configure(text=f"Clicks/s: {clicks} Score: {score} Timer: {timer}")
 
-    self.statsLabel = tk.Label(self.root, text=f"Clicks/s: {self.clicks} Score: {self.score} Timer: {self.timer}", font=('Comic Sans MS', 14), background="#BFACE2", foreground="#18122B", borderwidth=5)
-    self.statsLabel.pack(pady=5)
+def updateTimer():
+  global CPSOn, timer, root
+  root.after(1000, updateTimer)#Call update every second
+  if CPSOn == True:
+    timer -= 1
+    print(timer)
+    time.sleep(1)
 
-    #Set columns for buttons in one row
-    self.timerButtonFrame = tk.Frame(self.root)
-    self.timerButtonFrame.columnconfigure(0, weight=1)
-    self.timerButtonFrame.columnconfigure(1, weight=1)
-    self.timerButtonFrame.columnconfigure(2, weight=1)
-    self.timerButtonFrame.columnconfigure(3, weight=1)
+def calculateCPS():
+  pass
 
-    #Timer Buttons
-    self.oneSecBtn = tk.Button(self.timerButtonFrame, text="1 Second", font=('Comic Sans MS', 10), background="#645CBB", foreground="#18122B", activebackground="#A084DC",command=lambda:self.changeTimer(1))
-    self.oneSecBtn.grid(row=0, column=0, sticky=tk.W+tk.E)
+def showResult():
+  global clicks
+  messagebox.showinfo(title="Results",message=f"Your average Clicks Per Seconds are {clicks}")
 
-    self.fiveSecBtn = tk.Button(self.timerButtonFrame, text="5 Seconds", font=('Comic Sans MS', 10), background="#645CBB", foreground="#18122B", activebackground="#A084DC",command=lambda:self.changeTimer(5))
-    self.fiveSecBtn.grid(row=0, column=1, sticky=tk.W+tk.E)
+#Prevent accidental closing
+def on_closing():
+  if messagebox.askyesno(title="Quit?", message="Are you sure you wanna quit now ?"):
+    root.destroy()
 
-    self.tenSecBtn = tk.Button(self.timerButtonFrame, text="10 Seconds", font=('Comic Sans MS', 10), background="#645CBB", foreground="#18122B", activebackground="#A084DC",command=lambda:self.changeTimer(10))
-    self.tenSecBtn.grid(row=0, column=2, sticky=tk.W+tk.E)
+#Texts
+title = tk.Label(root, text="Clicks-per-Second", font=('Comic Sans MS', 20), background="#BFACE2", foreground="#18122B", borderwidth=5)
+title.pack(pady=15)
 
-    self.fifthTeenSecBtn = tk.Button(self.timerButtonFrame, text="15 Seconds", font=('Comic Sans MS', 10), background="#645CBB", foreground="#18122B", activebackground="#A084DC",command=lambda:self.changeTimer(15))
-    self.fifthTeenSecBtn.grid(row=0, column=3, sticky=tk.W+tk.E)
+statsLabel = tk.Label(root, text=f"Clicks/s: {clicks} Score: {score} Timer: {timer}", font=('Comic Sans MS', 14), background="#BFACE2", foreground="#18122B", borderwidth=5)
+statsLabel.pack(pady=5)
 
-    self.timerButtonFrame.pack(fill='x', padx=10, pady=10)
+#Set columns for buttons in one row
+timerButtonFrame = tk.Frame(root)
+timerButtonFrame.columnconfigure(0, weight=1)
+timerButtonFrame.columnconfigure(1, weight=1)
+timerButtonFrame.columnconfigure(2, weight=1)
+timerButtonFrame.columnconfigure(3, weight=1)
 
-    #Click button
-    self.clickButton = tk.Button(self.root, text="Click to Start", font=("Comic Sans MS", 16), background="#BFACE2", foreground="#18122B", activebackground="#674188",command=self.updateStats)
-    self.clickButton.pack(pady=20)
+#Timer Buttons
+oneSecBtn = tk.Button(timerButtonFrame, text="1 Second", font=('Comic Sans MS', 10), background="#645CBB", foreground="#18122B", activebackground="#A084DC",command=lambda:changeTimer(1))
+oneSecBtn.grid(row=0, column=0, sticky=tk.W+tk.E)
 
-    self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
-    self.root.mainloop()
+fiveSecBtn = tk.Button(timerButtonFrame, text="5 Seconds", font=('Comic Sans MS', 10), background="#645CBB", foreground="#18122B", activebackground="#A084DC",command=lambda:changeTimer(5))
+fiveSecBtn.grid(row=0, column=1, sticky=tk.W+tk.E)
 
-  def updateStats(self):
-    if self.timer >= 0:
-      self.CPSOn = True
-      self.score += 1
-      self.statsLabel.configure(text=f"Clicks/s: {self.clicks} Score: {self.score} Timer: {self.timer}")
-      self.calculateCPS()
-    elif self.timer == 0:
-      self.CPSOn = False
+tenSecBtn = tk.Button(timerButtonFrame, text="10 Seconds", font=('Comic Sans MS', 10), background="#645CBB", foreground="#18122B", activebackground="#A084DC",command=lambda:changeTimer(10))
+tenSecBtn.grid(row=0, column=2, sticky=tk.W+tk.E)
 
-  #Change Timer Time
-  def changeTimer(self, time):
-    self.timer = time
-    self.statsLabel.configure(text=f"Clicks/s: {self.clicks} Score: {self.score} Timer: {self.timer}")
+fifthTeenSecBtn = tk.Button(timerButtonFrame, text="15 Seconds", font=('Comic Sans MS', 10), background="#645CBB", foreground="#18122B", activebackground="#A084DC",command=lambda:changeTimer(15))
+fifthTeenSecBtn.grid(row=0, column=3, sticky=tk.W+tk.E)
 
-  def updateTimer(self):
-    if self.CPSOn == True:
-      self.timer -= 1
-      time.sleep(1)
-      self.root.after(1000, self.updateTimer)#Call update every second
+timerButtonFrame.pack(fill='x', padx=10, pady=10)
 
-  def calculateCPS(self):
-    pass
+#Click button
+clickButton = tk.Button(root, text="Click to Start", font=("Comic Sans MS", 16), background="#BFACE2", foreground="#18122B", activebackground="#674188",command=updateStats)
+clickButton.pack(pady=20)
 
-  def showResult(self):
-    messagebox.showinfo(title="Results",message=f"Your average Clicks Per Seconds are {self.clicks}")
-
-  #Prevent accidental closing
-  def on_closing(self):
-    if messagebox.askyesno(title="Quit?", message="Are you sure you wanna quit now ?"):
-      self.root.destroy()
-
-if __name__ == "__main__":
-  app = GUI()
-  app.mainloop()
+updateTimer()
+root.protocol("WM_DELETE_WINDOW", on_closing)
+root.mainloop()
